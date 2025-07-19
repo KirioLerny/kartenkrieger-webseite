3b20424be6bac854f500d091e8595ca90b840eab
KirioLerny <nozd-0a2f@stud.hhs.karlsruhe.de>
2025-07-19 04:31:16 +0200

    Added Components and Styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ShowcaseComponent} from './showcase/showcase.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ShowcaseComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
