87186ca116950865005e45bd04f5734e725ad37c
Kirio <nozd-0a2f@stud.hhs.karlsruhe.de>
2026-01-16 20:28:28 +0100

    add FAQ and color palette components with navigation links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,15 @@ import { HomeComponent } from './home/home.component';
 import { ShowcaseComponent } from './showcase/showcase.component';
 import { ImpressumComponent } from './impressum/impressum.component';
 import { DatenschutzComponent } from './datenschutz/datenschutz.component';
+import { FarbpaletteComponent } from './farbpalette/farbpalette.component';
+import { FaqComponent } from './faq/faq.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'galerie', component: ShowcaseComponent },
+  { path: 'farbpalette', component: FarbpaletteComponent },
+  { path: 'faq', component: FaqComponent },
   { path: 'impressum', component: ImpressumComponent },
   { path: 'datenschutz', component: DatenschutzComponent },
-  { path: '**', redirectTo: '', pathMatch: 'full' } // Fallback zur Home
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
